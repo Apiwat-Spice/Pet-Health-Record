41757271106b0e06276465b14b490dbc2688b809
Apiwat-Spice <xkhunbumrung@gmail.com>
2026-06-24 14:04:22 +0700

    ลบขีด

--- src/screens/Welcome/WelcomeScreen.js
+++ src/screens/Welcome/WelcomeScreen.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Welcome-Screen() {
+export default function WelcomeScreen() {
   return (
     <View style={styles.container}>
       <Text>PetHealthRecord</Text>
